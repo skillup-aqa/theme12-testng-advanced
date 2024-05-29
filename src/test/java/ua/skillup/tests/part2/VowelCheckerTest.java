e254fa7ca9e9c49ac8dc7e5ae9ca932c131e5830
Tetiana Yakymiuk <t882005@yahoo.com>
2024-05-29 18:36:17 -0400

    Adding tests to part2 VowelChecker

--- src/test/java/ua/skillup/tests/part2/VowelCheckerTest.java
+++ src/test/java/ua/skillup/tests/part2/VowelCheckerTest.java
@@ -1,6 +1,5 @@
 package ua.skillup.tests.part2;
 
-import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ua.skillup.part2.VowelChecker;
@@ -12,17 +11,21 @@ public class VowelCheckerTest {
     public Object[] vowels() {
         return new Object[]{'a', 'e', 'o', 'u', 'i'};
         }
-
+    @DataProvider(name = "notvowels")
+    public Object[] notVowels() {
+        return new Object[]{'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l',
+                'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x',
+                'y', 'z'};
+    }
 
     @Test(dataProvider = "vowels")
     public void isVowelPositive(char letter) {
         assertTrue(VowelChecker.isVowel(letter));
     }
 
-    @Test(dataProvider = "notVowels")
+    @Test(dataProvider = "notvowels")
     public void isVowelNegative(char letter) {
         assertFalse(VowelChecker.isVowel(letter));
     }
-
 }
 
